14249605271f39b69f1ecfcc5800421f0f6592bd
kokisalvarez <adolfo.alvarez@alumnos.udg.mx>
2025-07-11 01:46:36 -0600

    fix: make vite use process.env.PORT for vercel dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    // Usa la variable PORT si existe, si no, 5173
+    port: Number(process.env.PORT) || 5173,
+  },
 
 // https://vitejs.dev/config/
 export default defineConfig({
