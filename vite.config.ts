970cd6482594f852d478ba4389d3b78a4c4a4082
kokisalvarez <adolfo.alvarez@alumnos.udg.mx>
2025-07-11 01:49:04 -0600

    fix: make vite use process.env.PORT for vercel dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
 
-export default defineConfig({
-  server: {
-    // Usa la variable PORT si existe, si no, 5173
-    port: Number(process.env.PORT) || 5173,
-  },
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    // Usa el PUERTO que Vercel CLI asigne o, si no existe, 5173
+    port: Number(process.env.PORT) || 5173,
+    // StrictPort evita que Vite suba a otro puerto si el asignado está ocupado
+    strictPort: true,
   },
 });
