459c4e7e32365c82b156f4eb6cb2479167d4cfa0
kokisalvarez <adolfo.alvarez@alumnos.udg.mx>
2025-07-16 21:15:03 -0600

    debug: print Firestore env-vars & add debug-env endpoint

--- api/debug-env.ts
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    projectId: process.env.FIRESTORE_PROJECT_ID,
-    clientEmail: process.env.FIRESTORE_CLIENT_EMAIL,
-    hasKey: Boolean(process.env.FIRESTORE_PRIVATE_KEY)
-  });
-}
